939c6d7499944a70de86bae392584e22b4ac9c19
ecurzola <146590441+ecurzola@users.noreply.github.com>
2023-11-01 03:58:44 +0000

    completed

---
+++ app/controllers/dice_controller.rb
@@ -0,0 +1,21 @@
+class DiceController < ApplicationController
+
+  def homepage
+    render({:template => "dice_templates/homepage"})
+  end
+
+  def toss
+    
+    @num_dice = params.fetch("number_of_dice").to_i
+    @sides = params.fetch("how_many_sides").to_i
+  
+    @rolls = []
+  
+    @num_dice.times do
+      die = rand(1..@sides)
+      @rolls.push(die)
+    end
+
+    render({:template => "dice_templates/outcome"})
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
 
+  get("/dice/:number_of_dice/:how_many_sides", {:controller => "dice", :action => "toss"})
+  get("/", {:controller => "dice", :action => "homepage"})
+
 end
